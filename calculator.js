f77f350bb8fe44969fbd19584461fea9fa16573d
seaweed78 <adhikarikshitiz12@gmail.com>
2023-06-24 15:32:48 +0545

    bmiCalculator added

--- calculator.js
+++ calculator.js
@@ -32,3 +32,30 @@ app.post("/", function (req, res) {
 
   res.send(n1 + " " + op + " " + n2 + " = " + result);
 });
+
+app.get("/bmicalculator", function (req, res) {
+  res.sendFile(__dirname + "\\bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function (req, res) {
+  let weight = req.body.weight;
+  let height = req.body.height;
+
+  const bmiInfo = calculateBMI(weight, height);
+  let bmi = bmiInfo.bmi;
+  let condition = bmiInfo.condition;
+  res.send("BMI = " + bmi + " Condition = " + condition);
+});
+
+function calculateBMI(weight, height) {
+  let heightInMeters = height * 0.3048;
+  let bmi = weight / (heightInMeters ^ 2);
+  let condition = "";
+  bmi = bmi.toFixed(2);
+
+  if (bmi < 18.5) condition = "underweight";
+  else if (bmi >= 18.5 && bmi < 24.9) condition = "Healthy";
+  else if (bmi >= 24.9 && bmi < 30) condition = "overweight";
+  else if (bmi >= 30) condition = "Suffering from Obesity";
+  return { bmi, condition };
+}
